6c861879cd82e86eeee7787a9c8ba84d731f0a78
kim_haaga <kim.haaga@live.se>
2022-03-30 19:23:00 +0200

    Add responsive modal, Wip make it close after click, Wip make current page indicator

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,33 +1,59 @@
 import Image from 'next/image'
 import Link from 'next/link'
-import React from 'react'
 
 const Navbar = () => {
   return (
     <div className='navbar'>
-      <Link href='#' passHref>
-        <Image
-          src='/logo-with-text.svg'
-          height={121}
-          width={388}
-          as='img/svg'
-          alt='Timotuz logo'
-        />
-      </Link>
+      <input type='checkbox' id='burger' />
+      <div className='navbar__image'>
+        <Link href='/' passHref>
+          <Image
+            src='/logo-with-text.svg'
+            height={121}
+            width={388}
+            as='img/svg'
+            alt='Timotuz logo'
+          />
+        </Link>
+      </div>
 
+      <div className='navbar__burger'>
+        <label htmlFor='burger'>
+          <span></span>
+          <span></span>
+          <span></span>
+        </label>
+      </div>
       <div className='navbar__links'>
-        <Link href='#' passHref>
-          <a className='navbar__links--link'>Hem</a>
-        </Link>
-        <Link href='/properties' passHref>
-          <a className='navbar__links--link'>Fastigheter</a>
-        </Link>
-        <Link href='/aboutUs' passHref>
-          <a className='navbar__links--link'>Om oss</a>
-        </Link>
-        <Link href='/contactUs' passHref>
-          <a className='navbar__links--link'>Kontakt</a>
-        </Link>
+        <div className='navbar__links--link'>
+          <Link href='/' passHref>
+            <a className='navbar__links--a'>Hem</a>
+          </Link>
+        </div>
+        <div className='navbar__links--link'>
+          <Link href='/properties' passHref>
+            <a className='navbar__links--a'>Fastigheter</a>
+          </Link>
+        </div>
+        <div className='navbar__links--link'>
+          <Link href='/aboutUs' passHref>
+            <a className='navbar__links--a'>Om oss</a>
+          </Link>
+        </div>
+        <div className='navbar__links--link'>
+          <Link href='/contactUs' passHref>
+            <a className='navbar__links--a'>Kontakt</a>
+          </Link>
+        </div>
+        <div className='navbar__links--image'>
+          <Image
+            src='/logo.webp'
+            height={446}
+            width={392}
+            as='img/svg'
+            alt='Timotuz logo'
+          />
+        </div>
       </div>
     </div>
   )
