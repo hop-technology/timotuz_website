c92172741c4bdc0840ac8fa780a0571d31fe4056
kim_haaga <kim.haaga@live.se>
2022-03-31 16:04:26 +0200

    WIP fix image to fit container on small devices

--- components/IndexCarousel.jsx
+++ components/IndexCarousel.jsx
@@ -2,6 +2,7 @@ import React, { useEffect } from 'react'
 import useEmblaCarousel from 'embla-carousel-react'
 import Autoplay from 'embla-carousel-autoplay'
 import Image from 'next/image'
+import Link from 'next/link'
 
 const IndexCarousel = () => {
   const [emblaRef, emblaApi] = useEmblaCarousel({ loop: false }, [Autoplay()])
@@ -13,23 +14,36 @@ const IndexCarousel = () => {
   }, [emblaApi])
 
   return (
-    <div className='embla' ref={emblaRef}>
-      <div className='embla__container'>
-        <div className='embla__slide'>
-          <Image
-            src='/street1440p.webp'
-            alt='couple in living room'
-            height={1440}
-            width={2560}
-          />
+    <div className='index-carousel'>
+      <div className='embla' ref={emblaRef}>
+        <div className='embla__container'>
+          <div className='embla__slide'>
+            <Image
+              src='/street1440p.webp'
+              alt='Gatu vy nyproduktionsprojektet Aurora'
+              // height={1440}
+              // width={2560}
+              layout='fill'
+            />
+          </div>
+          <div className='embla__slide'>
+            <Image
+              src='/court1440p.webp'
+              alt='Gården till nyproduktionsprojektet Aurora'
+              // height={1440}
+              // width={2560}
+              layout='fill'
+            />
+          </div>
         </div>
-        <div className='embla__slide'>
-          <Image
-            src='/court1440p.webp'
-            alt='couple in living room'
-            height={1440}
-            width={2560}
-          />
+        <div className='embla__overlay'>
+          <h1 className='embla__overlay--title'>Nyproduktionsprojekt Aurora</h1>
+          <h2 className='embla__overlay--sub-title'>
+            Gamla anor möter morgondagens vanor
+          </h2>
+          <Link href='/properties' passHref>
+            <button className='embla__overlay--btn'>Läs mer</button>
+          </Link>
         </div>
       </div>
     </div>
