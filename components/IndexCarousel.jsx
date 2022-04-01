e921928df156e993281175e8e0d93c08b5558728
kim_haaga <kim.haaga@live.se>
2022-04-01 12:31:27 +0200

    Add image carousel for index

--- components/IndexCarousel.jsx
+++ components/IndexCarousel.jsx
@@ -1,48 +1,57 @@
 import React, { useEffect } from 'react'
-import useEmblaCarousel from 'embla-carousel-react'
-import Autoplay from 'embla-carousel-autoplay'
 import Image from 'next/image'
 import Link from 'next/link'
 
 const IndexCarousel = () => {
-  const [emblaRef, emblaApi] = useEmblaCarousel({ loop: false }, [Autoplay()])
-
-  useEffect(() => {
-    if (emblaApi) {
-      // Embla API is ready
-    }
-  }, [emblaApi])
-
   return (
     <div className='index-carousel'>
-      <div className='embla' ref={emblaRef}>
-        <div className='embla__container'>
-          <div className='embla__slide'>
-            <Image
-              src='/street1440p.webp'
-              alt='Gatu vy nyproduktionsprojektet Aurora'
-              // height={1440}
-              // width={2560}
-              layout='fill'
-            />
-          </div>
-          <div className='embla__slide'>
-            <Image
-              src='/court1440p.webp'
-              alt='Gården till nyproduktionsprojektet Aurora'
-              // height={1440}
-              // width={2560}
-              layout='fill'
-            />
-          </div>
+      <div className='index-carousel__large'>
+        <div className='image-container'>
+          <Image
+            src='/street1440p.webp'
+            alt='Gatuvy nyproduktionsprojektet Aurora'
+            height={1080}
+            width={1920}
+            layout='responsive'
+          />
         </div>
-        <div className='embla__overlay'>
-          <h1 className='embla__overlay--title'>Nyproduktionsprojekt Aurora</h1>
-          <h2 className='embla__overlay--sub-title'>
-            Gamla anor möter morgondagens vanor
-          </h2>
+        <div className='image-container'>
+          <Image
+            src='/court1440p.webp'
+            alt='Gården till nyproduktionsprojektet Aurora'
+            height={1080}
+            width={1920}
+            layout='responsive'
+          />
+        </div>
+      </div>
+      <div className='index-carousel__small'>
+        <div className='image-container'>
+          <Image
+            src='/street1440p.webp'
+            alt='Gatuvy nyproduktionsprojektet Aurora'
+            height={500}
+            width={700}
+            layout='responsive'
+          />
+        </div>
+        <div className='image-container'>
+          <Image
+            src='/court1440p.webp'
+            alt='Gården till nyproduktionsprojektet Aurora'
+            height={500}
+            width={700}
+            layout='responsive'
+          />
+        </div>
+      </div>
+
+      <div className='index-carousel__overlay'>
+        <div className='index-carousel__overlay--content'>
+          <h1 className='title'>Nyproduktionsprojekt Aurora</h1>
+          <h2 className='sub-title'>Gamla anor möter morgondagens vanor</h2>
           <Link href='/properties' passHref>
-            <button className='embla__overlay--btn'>Läs mer</button>
+            <button className='btn'>Läs mer</button>
           </Link>
         </div>
       </div>
