9a21aa86f6d8023f92e53056ef71592e7bcf0a7f
kim_haaga <kim.haaga@live.se>
2022-04-05 15:56:39 +0200

    commetn out a link in indexgrid

--- components/IndexGrid.jsx
+++ components/IndexGrid.jsx
@@ -21,50 +21,46 @@ const IndexGrid = () => {
             </Link>
           </div>
         </div>
+        {/* <Link href='/fastigheter' passHref> */}
+        <div className='aurora'>
+          <Image
+            src='/court-grid.webp'
+            height={574}
+            width={346}
+            alt='Innegård på nyproduktion Aurora'
+            layout='responsive'
+          />
+
+          <div className='aurora__content'>
+            <p className='aurora__content--top-text'>Nyproduktion</p>
+            <p className='aurora__content--bot-text'>Aurora</p>
+          </div>
+        </div>
+        {/* </Link> */}
         <Link href='/fastigheter' passHref>
-          <div className='aurora'>
-            <>
-              <Image
-                src='/court-grid.webp'
-                height={574}
-                width={346}
-                alt='Innegård på nyproduktion Aurora'
-                layout='responsive'
-              />
-            </>
-            <div className='aurora__content'>
-              <p className='aurora__content--top-text'>Nyproduktion</p>
-              <p className='aurora__content--bot-text'>Aurora</p>
+          <div className='ystad'>
+            <Image
+              src='/ystad-grid.webp'
+              height={248}
+              width={346}
+              alt='Fågelvy över fastighet i Ystad'
+              layout='responsive'
+            />
+            <div className='ystad__content'>
+              <p className='ystad__content--top-text'>Ystad</p>
             </div>
           </div>
         </Link>
         <Link href='/fastigheter' passHref>
-          <>
-            <div className='ystad'>
-              <Image
-                src='/ystad-grid.webp'
-                height={248}
-                width={346}
-                alt='Fågelvy över fastighet i Ystad'
-                layout='responsive'
-              />
-              <div className='ystad__content'>
-                <p className='ystad__content--top-text'>Ystad</p>
-              </div>
-            </div>
-          </>
-        </Link>
-        <Link href='/fastigheter' passHref>
           <div className='malmo'>
-            <a>
-              <Image
-                src='/limhamn-grid.webp'
-                height={248}
-                width={346}
-                alt='Fågelvy över hus i Limhamn'
-                layout='responsive'
-              />
-            </a>
+            <Image
+              src='/limhamn-grid.webp'
+              height={248}
+              width={346}
+              alt='Fågelvy över hus i Limhamn'
+              layout='responsive'
+            />
+
             <div className='malmo__content'>
               <p className='malmo__content--top-text'>Malmö/Limhamn</p>
             </div>
